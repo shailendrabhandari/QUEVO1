c542461b949917a5ef6ab64eed825812f624dba3
Shailendra995 <shailendra.vandari@gmail.com>
2023-02-14 07:55:44 +0100

    first commit

--- Quevo/Circuit.py
+++ Quevo/Circuit.py
@@ -1,21 +1,11 @@
-#  Copyright ""Bhandari, S., Overskott, S., Adamopoulos, I., Lind, P.G., Denysov,
-#    S., Nichele, S. (2022). Evolving Quantum Circuits to Implement Stochastic and 
-#    Deterministic Cellular Automata Rules. In: Chopard, B., Bandini, S., Dennunzio, A.,
-#    Arabi Haddad, M. (eds) Cellular Automata. ACRI 2022. Lecture Notes in Computer 
-#    Science, vol 13402. Springer, Cham. https://doi.org/10.1007/978-3-031-14926-9_11""
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
 
+import qutip
+from qiskit.quantum_info import partial_trace
+from qiskit.visualization import circuit_drawer
+from scipy.special import comb
+import itertools
+import typing
+import numpy as np
 from typing import List
 from qiskit import QuantumCircuit, Aer, assemble
 from scipy.special import rel_entr
@@ -35,7 +25,7 @@ class Circuit(object):
     _SHOTS (int):
         Number of runs in the qiskit quantum _circuit simulator.
     _STARTING_STATES (List[list]):
-        Possible cellular automata initial conditions for the 1D Von Neumann neighborhood.
+        a list of all possible starting states for the 3 qubits.
     """
 
     def __init__(self, chromosome: Chromosome):
@@ -49,7 +39,7 @@ class Circuit(object):
             The chromosome that describes the QuantumCircuit.
         """
         self.chromosome = chromosome
-        self._circuit = QuantumCircuit(3, 1)
+        self._circuit = QuantumCircuit(3, 1) #three qubits and 1 classical bit
         self._SHOTS = 2048
         self._STARTING_STATES = [[0, 0, 0],
                                  [0, 0, 1],
@@ -61,13 +51,24 @@ class Circuit(object):
                                  [1, 1, 1]]
         self.results = {}
 
+
     def __repr__(self):
         """Returns a string visualizing the quantum _circuit"""
         return self.draw()
 
     def generate_circuit(self) -> None:
+
+        
         """
         Parses the chromosome, and generates a Qiskit QuantumCircuit from it.
+
+	The generate_circuit method generates a Qiskit quantum circuit from a chromosome object.
+	The chromosome object has a list of integers and a dictionary that maps the integers to different
+	quantum gates, such as 'h', 'cx', 'x', 'swap', 'rzz', 'rxx', 'toffoli', 'y', and 'z'. The method
+	 creates a circuit by iterating through the integer list in steps of 3 and using each group of 3
+	 integers to determine which gate to apply, to which qubits, and with what parameters. The method then
+	 adds the chosen gate to the circuit. Finally, it adds a measurement gate to the first
+	 qubit and outputs qubit 0.
         """
         gates = int(self.chromosome.get_length() / 3)
 
@@ -109,159 +110,86 @@ class Circuit(object):
             else:
                 print(gate + " is not a valid gate!")
 
-        self._circuit.measure(0, 0)
+        self._circuit.measure(0, 0) 
 
-    def calculate_probability_of_one(self) -> float:
-        """Returns the measured chance of one after simulation"""
-        counts = self.run_simulator()
-        if '1' in counts:
-            chance_of_one = counts['1'] / self._SHOTS
-        else:
-            chance_of_one = 0.0
 
-        return chance_of_one
-
-    def find_chromosome_fitness(self, desired_chance_of_one: List[float]) -> float:
+    def compute_MW_entanglement(self, ket):
         """
-        Calculates and return the fitness for the chromosome i.e.
-        the sum of differences in all initial states.
-
-        Parameters
-        ----------
-        desired_chance_of_one: List[float]
-            A list of desired probabilities for all the CA initial states.
-
-        Returns
-        -------
-        fitness: (float)
-            The chromosome fitness score.
+        Compute the Mayer-Wallach measure of entanglement.
+        
+        Parameters:
+        -----------
+        ket : numpy.ndarray or list
+            Vector of amplitudes in 2**N dimensions
+        
+        Returns:
+        --------
+        MW_entanglement : float
+            Mayer-Wallach entanglement value for the input ket
         """
-        fitness = 0
-        for i in range(0, len(self._STARTING_STATES)):
+        ket = qutip.Qobj(ket, dims=[[2]*(3), [2]*(3)]).unit()
+        entanglement_sum = 2
+        for k in range(3):
+            rho_k_sq = ket.ptrace([k])**2
+            entanglement_sum += rho_k_sq.tr()
+   
+        MW_entanglement = 2*(1 - (1/3)*entanglement_sum)
+        return MW_entanglement
 
-            state = self._STARTING_STATES[i]
-            probability = desired_chance_of_one[i]
-            found_probability = self.find_init_state_probability(state)
-            difference = abs(probability - found_probability)  
-            fitness = fitness + difference
-
-        return fitness
-
-    def find_kullback_liebler_fitness(self, desired_chance_of_one: List[float]) -> float:
+    def find_chromosome_fitness(self, state: List[int]) -> float:
         """
-        Calculates and return the fitness for the chromosome as relative entropy (Kullback-Liebler).
-
-        Parameters
-        ----------
-        desired_chance_of_one: List[float]
-            A list of desired probabilities for all the CA initial states.
-
-        Returns
-        -------
-        fitness: (float)
-            The the Kullback-Liebler divergence as chromosome fitness score.
-
+        Compute the fitness of a chromosome by computing the average Mayer-Wallach
+        entanglement for all the starting states.
+        
+        Parameters:
+        -----------
+        state : list
+            A binary string of length 3 representing the state of the qubits
+            
+        Returns:
+        --------
+        avg_MW_entanglement : float
+            The average Mayer-Wallach entanglement for all starting states
         """
-        fitness = 0
-        probabilities = []
-        found_probabilities = []
-        for i in range(0, len(self._STARTING_STATES)):
 
-            state = self._STARTING_STATES[i]
-            found_probabilities.append(self.find_init_state_probability(state))
-            probabilities.append(desired_chance_of_one[i])
-
-        p = [x if x != 0 else 0.0001 for x in found_probabilities]
-        q = [x if x != 0 else 0.0001 for x in probabilities]
-
-        d = sum(rel_entr(p, q))
-
-        fitness = fitness + d
-
-        return fitness
-
-    def find_init_state_probability(self, state: List[int]) -> float:
-        """
-        Finds the difference between the desired probability and measured probability for given state.
-
-        Parameters
-        ---------
-        state: (List[int])
-            A list with a binary triplet describing the initial state.
-
-        Returns
-        -------
-        difference: (float)
-            The difference between the desired probability and measured probability for given state.
-        """
-        self.clear_circuit()
-        self.initialize_initial_states(state)
         self.generate_circuit()
 
-        chance_of_one = self.calculate_probability_of_one()
-        return chance_of_one
+        MW_entanglement_values = []
+        fitness = 0
+        MW_entanglement = self.compute_MW_entanglement(ket=state)
+        print('MW_entanglement = {}\n'.format(MW_entanglement))
+        fitness = 0 + MW_entanglement
+        return fitness
+ 
+    '''@staticmethod
+    def scott_helper(state, perms):
+        """Helper function for entanglement measure. It gives trace of the output state"""
+        dems = np.linalg.matrix_power(
+            [partial_trace(state, list(qb)).data for qb in perms], 2
+        )
+        trace = np.trace(dems, axis1=1, axis2=2)
+        return np.sum(trace).real
 
-    def print_ca_outcomes(self, desired_chance_of_one: List[float]):
-        """Prints a table of the results from a run of the chromosome"""
-        print("Initial State | Desired outcome | Actual outcome  | Difference")
-        total_diff = 0
-        for i in range(0, len(self._STARTING_STATES)):
-            state = self._STARTING_STATES[i]
-            probability = desired_chance_of_one[i]
+    def find_chromosome_fitness(self, states):
+        r"""Returns the meyer-wallach entanglement measure for the given circuit.
 
-            found_probability = self.find_init_state_probability(state)
-            difference = abs(found_probability - probability)
-            total_diff = total_diff + difference
-            desired_format = "{:.4f}".format(desired_chance_of_one[i])
-            chance_format = "{:.4f}".format(found_probability)
-            diff_format = "{:.4f}".format(difference)
+        .. math::
+            Q = \frac{2}{|\vec{\theta}|}\sum_{\theta_{i}\in \vec{\theta}}
+            \Bigg(1-\frac{1}{n}\sum_{k=1}^{n}Tr(\rho_{k}^{2}(\theta_{i}))\Bigg)
 
-            print(str(self._STARTING_STATES[i]) + "           "
-                  + desired_format + "           "
-                  + chance_format + "           "
-                  + diff_format)
-            self.clear_circuit()
-        print("Total difference: " + str(total_diff))
-
-    def get_total_difference(self, desired_chance_of_one: List[float]):
-        total_diff = 0
-        for i in range(0, len(self._STARTING_STATES)):
-            state = self._STARTING_STATES[i]
-            probability = desired_chance_of_one[i]
-
-            found_probability = self.find_init_state_probability(state)
-            difference = abs(found_probability - probability)
-            total_diff = total_diff + difference
-
-        return total_diff
-
-    def print_counts(self):
-        """Prints the counts result from simulation"""
-        index = 0
-        for triplet in self._STARTING_STATES:
-            self.clear_circuit()
-            self.initialize_initial_states(triplet)
-            self.generate_circuit()
-            print(self.run_simulator())
-            index = index + 1
-
-    def initialize_initial_states(self, triplet: List[int]) -> None:
         """
-        Initializes a Cellular Automata (CA) state in the _circuit.
-
-        Parameters
-        ----------
-        triplet: List[int]
-            A list of three integer in {0,1} representing the one of the
-            eight starting possibilities in 1D Von Neumann CA.
-        """
-
-        if triplet[0] == 1:
-            self._circuit.x(0)
-        if triplet[1] == 1:
-            self._circuit.x(1)
-        if triplet[2] == 1:
-            self._circuit.x(2)
+        fitness = 0
+        permutations = list(itertools.combinations(range(3), 3 - 1))
+        ns = 2 * sum(
+            [
+                1 - 1 / 3 * self.scott_helper(state, permutations)
+                for state in states
+            ]
+        )
+        return ns.real
+        fitness = 0 + ns.real
+        print('MW_entanglement = {}\n'.format(ns))
+        return fitness'''
 
     def run_simulator(self) -> dict:
         """
